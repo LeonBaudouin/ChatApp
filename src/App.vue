707183e4a91fed31e3f799cdf2b30bc75be6e1ff
LeonBaudouin <leondedouin@gmail.com>
2019-09-17 19:11:00 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        <div>
+            <router-link to='/chat'>Chat</router-link>
+            <router-link to='/'>Login</router-link>
+        </div>
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
